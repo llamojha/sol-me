f8b16bd3ea9877e16d9bb1c0056082a89a3ea841
Alvaro Martinez Llamojha <amllamojha@gmail.com>
2022-08-16 12:35:38 +0100

    UX: Final touch for profile details

--- src/ui-components/ProfileDetailVariants.jsx
+++ src/ui-components/ProfileDetailVariants.jsx
@@ -37,7 +37,7 @@ export default function ProfileDetailVariants(props) {
     {
       overrides: {
         ProfileBanner: {
-          height: "240px",
+          height: "224px",
           alignSelf: "stretch",
           objectFit: "cover",
         },
@@ -45,15 +45,15 @@ export default function ProfileDetailVariants(props) {
         Username: {
           lineHeight: "50px",
           justifyContent: "flex-start",
-          width: "379px",
+          width: "355px",
+          basis: "355px",
           height: "50px",
-          basis: "379px",
         },
         Profile: {
           gap: "16px",
-          height: "86px",
-          alignItems: "flex-start",
-          padding: "0px 0px 16px 20px",
+          height: "92px",
+          padding: "16px 0px 16px 20px",
+          border: "1px SOLID rgba(255,255,255,1)",
         },
         DonationDescription: { alignSelf: "stretch", objectFit: "cover" },
         SolanaPayFrame: {
@@ -63,21 +63,39 @@ export default function ProfileDetailVariants(props) {
         },
         ProfileInfo: {
           gap: "24px",
-          padding: "16px 0px 16px 0px",
+          padding: "15px 11px 15px 11px",
+          height: "280px",
+          alignItems: "center",
           shrink: "0",
           objectFit: "cover",
         },
-        ProfileContent: { gap: "0", height: "484px", direction: "column" },
+        ProfileContent: {
+          gap: "12px",
+          alignItems: "center",
+          direction: "column",
+          overflow: "hidden",
+        },
         DescriptionTitle: {},
         DescriptionText: {},
         Description: {},
-        DescriptionFrame: { gap: "32px", width: "480px", shrink: "0" },
+        DescriptionFrame: {
+          gap: "32px",
+          height: "326px",
+          alignItems: "center",
+          shrink: "0",
+        },
         ProfileDetail: {
+          padding: "0px 0px 0px 0px",
           alignItems: "center",
           alignSelf: "stretch",
           objectFit: "cover",
         },
-        ProfileDetailVariants: { direction: "column", overflow: "hidden" },
+        ProfileDetailVariants: {
+          direction: "column",
+          width: "480px",
+          height: "834px",
+          overflow: "hidden",
+        },
       },
       variantValues: { variant: "Small" },
     },
@@ -94,6 +112,7 @@ export default function ProfileDetailVariants(props) {
       position="relative"
       padding="0px 0px 0px 0px"
       width="100%"
+      backgroundImage=" linear-gradient(hsl(332, 54%, 43%), hsl(331, 41%, 66%))"
       {...rest}
       {...getOverrideProps(overrides, "ProfileDetailVariants")}
     >
@@ -101,15 +120,14 @@ export default function ProfileDetailVariants(props) {
         gap="24px"
         direction="column"
         width="100%"
-        height="638px"
+        height="834px"
         shrink="0"
         position="relative"
-        padding="0px 0px 0px 0px"
+        padding="24px 24px 24px 24px"
         {...getOverrideProps(overrides, "ProfileDetail")}
       >
         <Flex
           gap="32px"
-          height="580px"
           alignItems="flex-start"
           shrink="0"
           alignSelf="stretch"
@@ -120,8 +138,8 @@ export default function ProfileDetailVariants(props) {
           {...getOverrideProps(overrides, "ProfileContent")}
         >
           <Image
-            width="780px"
-            height="580px"
+            width="700px"
+            height="600px"
             shrink="0"
             position="relative"
             padding="0px 0px 0px 0px"
@@ -131,12 +149,14 @@ export default function ProfileDetailVariants(props) {
           <Flex
             gap="48px"
             direction="column"
-            width="348px"
+            width="100%"
             grow="1"
-            basis="348px"
+            basis="380px"
             alignSelf="stretch"
             position="relative"
-            padding="64px 0px 64px 0px"
+            border="1px SOLID rgba(255,255,255,1)"
+            padding="63px 11px 63px 11px"
+            backgroundColor="hsl(331, 41%, 66%)"
             {...getOverrideProps(overrides, "ProfileInfo")}
           >
             <Flex
@@ -155,6 +175,7 @@ export default function ProfileDetailVariants(props) {
                 height="65px"
                 shrink="0"
                 position="relative"
+                border="1px SOLID rgba(255,255,255,1)"
                 borderRadius="160px"
                 padding="0px 0px 0px 0px"
                 src={user?.ProfileImage}
@@ -171,10 +192,9 @@ export default function ProfileDetailVariants(props) {
                 direction="column"
                 justifyContent="center"
                 letterSpacing="0.01px"
-                width="259px"
-                height="24px"
+                width="267px"
                 grow="1"
-                basis="259px"
+                basis="267px"
                 position="relative"
                 padding="0px 0px 0px 0px"
                 whiteSpace="pre-wrap"
@@ -211,16 +231,18 @@ export default function ProfileDetailVariants(props) {
           </Flex>
         </Flex>
         <Flex
-          gap="0"
+          gap="24px"
           direction="column"
-          height="34px"
+          height="162px"
           grow="1"
-          basis="34px"
+          basis="162px"
           alignSelf="stretch"
           objectFit="cover"
           position="relative"
-          padding="0px 0px 0px 0px"
+          border="1px SOLID rgba(255,255,255,1)"
+          padding="11px 11px 11px 11px"
           width="100%"
+          backgroundColor="hsl(331, 41%, 66%)"
           {...getOverrideProps(overrides, "DescriptionFrame")}
         >
           <Flex
@@ -256,7 +278,7 @@ export default function ProfileDetailVariants(props) {
               fontFamily="Inter"
               fontSize="16px"
               fontWeight="400"
-              color="rgba(92,102,112,1)"
+              color="rgba(0,0,0,1)"
               lineHeight="24px"
               textAlign="left"
               display="flex"
